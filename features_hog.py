9d331576e052467b7709a6879d83960fe4822142
unknown <tungduong156gli@gmail.com>
2025-12-18 17:18:39 +0700

    fix pipeline and fix ablation study

--- features_hog.py
+++ features_hog.py
@@ -6,8 +6,8 @@ def hog_feature(img_rgb_64: np.ndarray) -> np.ndarray:
     gray = rgb2gray(img_rgb_64)  # [64,64]
     feat = hog(
         gray,
-        orientations=9,
-        pixels_per_cell=(10,10),
+        orientations=6,
+        pixels_per_cell=(8,8),
         cells_per_block=(2,2),
         block_norm="L2-Hys",
         feature_vector=True
